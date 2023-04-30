755788a8821ef914870c5abe3d7d08d986d72235
rajadagada51 <97170585+rajadagada51@users.noreply.github.com>
2023-05-01 00:42:24 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 
 // Configure the database connection
 const pool = new Pool({
-  user: 'your_username',
-  host: 'your_host',
-  database: 'your_database_name',
-  password: 'your_password',
-  port: 5432, // Default PostgreSQL port is 5432
+  user: 'username',
+  host: 'host',
+  database: 'db_name',
+  password: 'password',
+  port: 5432,
 });
 
 // Fetch items from the database
